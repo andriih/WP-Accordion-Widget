5b5f0a65bc09c26e98e9c8217c43484b0413aa85
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2015-09-16 16:20:17 +0300

    scripts localized

--- awp-widget-cats.php
+++ awp-widget-cats.php
@@ -17,8 +17,11 @@ function awp_cats ()
 {
 	register_widget('AWP_Cats');
 }
+
 class AWP_Cats extends WP_Widget
 {
+	public $awp_cats_array;
+
 	function __construct()
 	{
 		$args = array(
@@ -34,18 +37,16 @@ class AWP_Cats extends WP_Widget
 		$eventType 	= '';
 		$hoverDelay = '';
 		$speed 		= '';
-		$exclude ='';
+		$exclude    = '';
 
 		extract($instance);
 
-		$title 		= !empty($title) ? esc_attr( $title ) : '' ;
-		$eventType  = !empty($eventType)  ? $eventType : 'click';
-		$hoverDelay = !empty($hoverDelay) ? $hoverDelay : 100;
-		$speed 		= !empty($speed) ? $speed : 50;
-		$exclude	= !empty($exclude) ? $exclude : '';
-
-	
-		print_r($eventType);
+		$title 		= !empty($title)      ? esc_attr( $title ) : '' ;
+		$eventType  = !empty($eventType)  ? $eventType 	       : 'click';
+		$hoverDelay = !empty($hoverDelay) ? $hoverDelay        : 100;
+		$speed 		= !empty($speed)      ? $speed             : 50;
+		$exclude	= !empty($exclude)    ? $exclude           : '';
+		
 		?>
 			<p>
 				<label for="<?php echo $this->get_field_id('title'); ?>">Заголовок:</label>
@@ -101,7 +102,18 @@ class AWP_Cats extends WP_Widget
 		$title='';
 		extract($args);
 		extract($instance);
-                add_action('wp_footer',array($this , 'awp_styles_scripts' ));
+		
+		$this->awp_cats_array = array (
+			'eventType' => 	$eventType  ,
+			'hoverDelay'=> 	$hoverDelay ,
+			'speed' 	=> 	$speed 
+		);
+
+		echo '<pre>';
+		print_r($instance);
+		echo '</pre>';
+
+        add_action('wp_footer',array($this , 'awp_styles_scripts' ));
                 
 		$title = apply_filters('widget_title', $title);
 		$cats = wp_list_categories(
@@ -122,16 +134,14 @@ class AWP_Cats extends WP_Widget
 	}
 
 	function awp_styles_scripts ()
-                {
-                    wp_register_script('awp-cookie', plugins_url('js/jquery.cookie.js',__FILE__),array('jquery'));
-                    wp_register_script('awp-hoverIntent', plugins_url('js/jquery.hoverIntent.minified.js',__FILE__),array('awp-cookie'));
-                    wp_register_script('awp-dcjqaccordion', plugins_url('js/jquery.dcjqaccordion.2.9.js',__FILE__),array('awp-hoverIntent'));
-                    wp_register_script('awp-scripts', plugins_url('js/awp-scripts.js',__FILE__),array('awp-dcjqaccordion'));
+    {
+        wp_register_script('awp-cookie', plugins_url('js/jquery.cookie.js',__FILE__),array('jquery'));
+        wp_register_script('awp-hoverIntent', plugins_url('js/jquery.hoverIntent.minified.js',__FILE__),array('awp-cookie'));
+        wp_register_script('awp-dcjqaccordion', plugins_url('js/jquery.dcjqaccordion.2.9.js',__FILE__),array('awp-hoverIntent'));
+        wp_register_script('awp-scripts', plugins_url('js/awp-scripts.js',__FILE__),array('awp-dcjqaccordion'));
 
-
-                  	wp_enqueue_script( 'awp-scripts') ; 
-
-                  	
-                }
+        wp_enqueue_script(  'awp-scripts') ;
+        wp_localize_script( 'awp-scripts', 'obj',  $this->awp_cats_array) ;               	
+    }
 }
 
--- js/awp-scripts.js
+++ js/awp-scripts.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
+	console.log(obj);
 	$('ul.accordion').dcAccordion({
+
 		eventType : 'click',
 		disableLink: true ,
 		hoverDelay : 100 ,
